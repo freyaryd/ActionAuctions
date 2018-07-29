48b85ffb04bb98ce2f46221ace9579a22c403114
Freya Ryd <flr26@cornell.edu>
2018-07-29 13:06:26 -0400

    reworked contract to allow auctioneers to create auctions

--- contracts/ActionAuctions.sol
+++ contracts/ActionAuctions.sol
@@ -6,38 +6,126 @@ import "./safemath.sol";
 
 contract ActionAuction{
 
-  address private topId;
-  uint private topBid = 0;
-  uint private total = 0;
-  uint16 private numBids = 0;
+  enum AuctionStatus {Active, Inactive}
 
-  function ActionAution() public payable{
+  struct Auction {
+    address auctioneer;
+    address charity;
 
+    string title;
+    AuctionStatus status;
+    uint numBids;
+
+    address topBidder;
+    uint topBid;
+    uint total;
+
+    uint uncollectedWinnings;
   }
 
-//depending on how is payment works, either return the top bidder info and total
-  function getPayment() private returns (address, uint){
-    address newId;
-    uint newBid;
-    if(numBids <= 16){
-      total = newBid.add(1);
-      //update top bid and top id
-      if(newBid > topBid){
-        topBid = newBid;
-        topId = newId;
-      }
+  Auction[] public auctions;          // All auctions
+
+  mapping(string => address) charities;
+
+  address auctioneer;
+
+  event AuctionCreated(uint auctionId, string title, address auctioneer);
+  event BidPlaced(address auctionId, address bidder, uint amt);
+  event AuctionEnded(string auctionId, address winner, uint amt);
+
+  modifier onlyAuctioneer(uint AuctionId){
+    if (auctioneer != msg.sender) throw;
+        _;
+  }
+
+  modifier onlyLive(uint AuctionID){
+    Auction a = auctions[auctionId];
+    if (a.status != AuctionStatus.Active) {
+      throw;
     }
-    //count bids
-    numBids = numsBids.add(1);
+    _;
   }
-}
 
-//figure out how to send payments
-function sendPayments(address _charity, address _winner, uint _amount) private payable{
-  //calc payout
-  uint payout = _amount / 2;
-  //send money to _charity
-  _charity.transfer(payout);
-  //send money to _winner
-  _winner.transfer(payout);
+  function ActionAution() public {
+    auctioneer = msg.sender;
+    //add the charity to mapping of approved charities
+    charities["amf"] =  0xD70AEeB15F5E934aCA7c626eA86bFc0ca5717C2A;
+  }
+
+  function createAuction(string _title, string _charity) returns (uint auctionId) {
+
+        // Make sure that the charity is correct
+        if (!charities[_charity].isValue()) {
+            LogFailure("Charity not valid");
+            throw;
+        }
+
+        auctionId = auctions.length++;
+        Auction a = auctions[auctionId];
+
+        a.auctioneer = msg.sender;
+        a.charity = charities[_charity];
+        a.title = _title;
+        a.status = AuctionStatus.Active;
+        a.numBids = 0;
+        a.topBidder;
+        a.topBid = 0;
+        a.total = 0;
+
+        AuctionCreated(auctionId, a.title, a.auctioneer);
+
+        return auctionId;
+    }
+
+  function placeBid(uint _auctionId) payable onlyLive(_auctionId) returns (bool success) {
+        Auction a = auctions[auctionId];
+        address newId = msg.sender;
+        uint amount = msg.value;
+
+        if(a.numBids <= 16){
+          a.total = SafeMath.add(a.newBid, 1);
+          //update top bid and top id
+          if(newBid > a.topBid){
+            a.topBid = newBid;
+            a.topId = newId;
+            uint winnings = SafeMath.div(newBid, 2);
+            a.uncollectedWinnings = SafeMath.add(a.uncollectedWinnings, winnings);
+          }
+        }
+        //count bids
+        a.numBids = SafeMath.add(a.numBids, 1);
+
+        if(a.numBids >= 16){
+          a.status = AuctionStatus.Inactive;
+        }
+
+        return true;
+    }
+
+  function getStatus(uint auctionId) public{
+    return auctions[auctionId].status;
+  }
+
+  //sends payment to charity when winner collects
+  function getWinnings(uint auctionId) public payable{
+    Auction a = auctions[auctionId];
+    //check auction is over
+    if (a.status = AuctionStatus.Active) {
+        LogFailure("Auction still running");
+        throw;
+    }
+
+    //set uncollectedWinnings to 0 to prevent receiving winnings mult. times
+    uint payout = a.uncollectedWinnings;
+    a.uncollectedWinnings = 0;
+    //send money to _charity
+    a.charity.transfer(payout);
+    //send money to _winner
+    a.topBidder.transfer(payout);
+  }
+
+  //Doesn't allow ether randomly being sent to you
+  function() {
+    throw;
+  }
 }
